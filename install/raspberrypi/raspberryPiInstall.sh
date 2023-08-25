d5137dbe4097b03a32b5666e7320ca7bb7e09653
Andrew DeVries <adevries@digitalexample.com>
2023-08-25 17:48:26 -0400

    install Node

--- install/raspberrypi/raspberryPiInstall.sh
+++ install/raspberrypi/raspberryPiInstall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash     
-#echo Add Node.JS 16 repo
-#curl -sSL https://deb.nodesource.com/setup_16.x | sudo bash -
-#echo Install Node.JS
-#catsudo apt install -y nodejs
+echo Add Node.JS 16 repo
+curl -sSL https://deb.nodesource.com/setup_16.x | sudo bash -
+echo Install Node.JS
+catsudo apt install -y nodejs
 
 sudo mkdir /opt/de
 sudo chown $(id -u):$(id -g) /opt/de
